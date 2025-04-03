94f8212a1b7b1ccae860653b5392761ea2d9fb40
Rohan More <rohanpmore1998@gmail.com>
2025-04-03 22:52:56 +0200

    Renderer 1st stage commit

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -7,6 +7,12 @@
 #include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/LayerStack.h"
 
+#include "Hazel/ImGui/ImGuiLayer.h"
+
+#include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/Buffer.h"
+#include "Hazel/Renderer/VertexArray.h"
+
 namespace Hazel {
 
 	class HAZEL_API Application
@@ -29,8 +35,18 @@ namespace Hazel {
 		bool OnWindowClose(WindowCloseEvent& e);
 		
 		std::unique_ptr<Window> m_Window;
+		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
+
+		//unsigned int m_VertexArray;
+		std::shared_ptr<Shader> m_Shader;
+		std::shared_ptr<VertexArray> m_VertexArray;
+		std::shared_ptr<VertexBuffer> m_VertexBuffer;
+		std::shared_ptr<IndexBuffer> m_IndexBuffer;
+	
+		std::shared_ptr<Shader> m_BlueShader;
+		std::shared_ptr<VertexArray> m_SquareVA;
 	private:
 		static Application* s_Instance;
 	};
